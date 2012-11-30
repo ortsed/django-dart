45c9ca504725257a949b2de54e5568b36a0cb8d7
Llewellyn Hinkes-Jones <lhinkesjones@theatlantic.com>
2012-11-30 14:57:22 -0500

    Seprating out the dart request method so it can be used in different situations

--- dart/tasks.py
+++ dart/tasks.py
@@ -3,9 +3,11 @@ from django.conf import settings
 from dart.models import Zone, DART_DOMAIN, Ad_Page
 from settings import DEBUG
 
+# String used to define a browser when making DART requests from a commandline
+DEFAULT_BROWSER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:8.0.1) Gecko/20100101 Firefox/8.0.1"
 
 
-def dart_sync(zones=None, user_agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:8.0.1) Gecko/20100101 Firefox/8.0.1", *args, **kwargs):
+def dart_sync(zones=None, *args, **kwargs):
 	""" 
 	Loops through all zones and positions and get's their status in doubleclick
 	then updates the local database with the result.
@@ -18,28 +20,16 @@ def dart_sync(zones=None, user_agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10.
 	for zone in zones:
 		positions = zone.zone_position_set.all()
 		for position in positions:
-			conn = httplib.HTTPConnection(DART_DOMAIN)
+		
 			size = position.position.size_list
 			pos = position.position.slug
-			ad_page = Ad_Page(zone=zone.slug, *args, **kwargs)
-			
+			ad_page = Ad_Page(zone=zone.slug, *args, **kwargs)	
 			url = ad_page.js_url(pos, with_ord=True, size=size) 
-			
 			
 			#if DEBUG: print url
 			
-			# HTTP request of a DART tag to DART server falsifying a browser request
-			conn.request(
-				"GET",
-				url,
-				headers={
-					"Referer": settings.SITE_URL, 
-					"User-Agent": user_agent
-				}
-			)
-			
-			
-			res = conn.getresponse()
+			res = dart_request(url, *args, **kwargs)
+
 			if res.status == 200:
 				response = res.read()
 
@@ -51,3 +41,20 @@ def dart_sync(zones=None, user_agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10.
 					position.enabled = True
 					#if DEBUG: print "Position: enabled"
 				position.save()
+				
+def dart_request(url, user_agent=DEFAULT_BROWSER_AGENT, domain=DART_DOMAIN, *args, **kwargs):
+	""" 
+	Makes a single HTTP request of a DART tag to DART 
+	server mimicking a browser request 
+	"""
+	conn = httplib.HTTPConnection(domain)
+	conn.request(
+		"GET",
+		url,
+		headers={
+			"Referer": settings.SITE_URL, 
+			"User-Agent": user_agent
+		}
+	)
+	return conn.getresponse()
+	
