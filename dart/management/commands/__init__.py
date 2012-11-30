6f351b02572c074b1606fbcac90d3457102bbd09
Llewellyn Hinkes-Jones <lhinkesjones@theatlantic.com>
2012-11-30 14:58:02 -0500

    Adding django management function to test dart URLs

---
+++ dart/management/commands/__init__.py
@@ -0,0 +1,2 @@
+VERSION_NAME = 'Dev'
+VERSION = (0, 2, 0)
---
+++ dart/management/commands/dart_request.py
@@ -0,0 +1,20 @@
+from django.core.management.base import BaseCommand, CommandError
+from dart.tasks import dart_request
+from optparse import make_option
+from urlparse import urlparse
+
+class Command(BaseCommand):
+	help = "Makes a URL request to DART and prints the resulting HTML"
+
+	
+	def handle(self, *args, **kwargs):
+		if len(args) == 0:
+			raise CommandError("No arguments specified.  URL needed to make a DART request")
+		
+		parsed_url = urlparse(args[0])
+		if parsed_url.netloc:
+			kwargs["domain"] = parsed_url.netloc
+			
+		response = dart_request(parsed_url.geturl(), *args, **kwargs)
+		
+		self.stdout.write("\nDART server response: \n\n" + response.read() + "\n\n")
